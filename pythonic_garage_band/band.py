5a2f32c71c0aea7c20391948f918fe2ce518fefb
rawnaq <engrawnaqaburummn@gmail.com>
2021-11-04 08:30:58 +0200

    eddit

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -3,59 +3,82 @@ from abc import ABC,abstractmethod
 
 
 class Musician:
-    def __init__(self,name):
+    """
+    Musician Class
+    """
+    def __init__(self,name,instrument='',solo=''):
         self.name = name
-    @abstractmethod
-    def __str__(self):
-        pass
-    @abstractmethod
-    def __repr__(self):
-        pass
-    @abstractmethod
-    def get_instrument():
-        pass
-    @abstractmethod
-    def play_solo():
-       pass
+        self.instrment = instrument
+        self.solo = solo
 
+    def get_instrument(self):
+        return self.instrument
+
+    def play_solo(self):
+        return self.solo
 
 
 class Guitarist(Musician):
+    """
+   Guitarist Class
+    """
     def __str__(self):
+        
         return f"My name is {self.name} and I play guitar"
+
+        
     def __repr__(self):
         return f'Guitarist instance. Name = {self.name}'
+
     def get_instrument(self):
         return 'guitar'
     def play_solo(self):
+
        return "face melting guitar solo"
 
 
 
 
 class Drummer(Musician):
+    """
+    Drummer Class
+    """
     def __str__(self):
         return f"My name is {self.name} and I play drums"
+
+
     def __repr__(self):
         return f"Drummer instance. Name = {self.name}"
+
+
     def get_instrument(self):
         return "drums"
+
+
+
     def play_solo(self):
+
+        
        return "rattle boom crash"
 
 
 
+
+
 class Bassist(Musician):
+    """
+    Bassist Class
+    """
+    def __init__(self,name):
+        self.name = name
+        self.instrument = 'bass'
+        self.solo = "bom bom buh bom" 
+
     def __str__(self):
         return f"My name is {self.name} and I play bass"
+
     def __repr__(self):
         return f"Bassist instance. Name = {self.name}"
-    def get_instrument(self):
-        return "bass"
-    def play_solo(self):
-       return "bom bom buh bom"
-
-
 
 
        
@@ -65,11 +88,18 @@ class Band:
         self.name=name
         self.members=members
         Band.instances.append(self)
+
+
+
     def play_solos(self):
-        arrayOfSolo = []
+        players = []
         for i in self.members:
-            arrayOfSolo.append(i.play_solo())
-        return arrayOfSolo
+
+            players.append(i.play_solo())
+            
+        return players
+
+        
     @classmethod
     def to_list(cls):
         return cls.instances
